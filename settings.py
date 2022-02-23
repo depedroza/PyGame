fe079f6953037b0d9fc11090a2a15d60a7c66079
Daniel Pedroza <Daniel_Pedroza1@baylor.edu>
2022-02-23 11:07:04 -0600

    group and fire bullets, change their color

--- settings.py
+++ settings.py
@@ -14,5 +14,5 @@ class Settings:
         # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
-        self.buullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_height = 15
+        self.bullet_color = (220, 20, 60)
