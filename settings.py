d2935ae429106eb1a2b99afc2111a6b689214634
Daniel Pedroza <Daniel_Pedroza1@baylor.edu>
2022-02-24 17:02:32 -0600

    fixed settings redundancy

--- settings.py
+++ settings.py
@@ -9,23 +9,18 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Bullet settings
         #   change width, speed, etc to test out features more easily
         #   or to create new powers for the ship
-        self.bullet_speed = 1.4
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (220, 20, 60)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
@@ -35,11 +30,13 @@ class Settings:
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
-        self.bullet_speed = 3.0
+        self.bullet_speed = 3.2
         self.alien_speed = 1.0
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        self.alien_points = 50
 
     def increase_speed(self):
         """Increase speed settings."""
