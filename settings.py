1888c499d1b3a6906ec68f951d3d57abc0b1badb
Daniel Pedroza <Daniel_Pedroza1@baylor.edu>
2022-02-24 17:17:11 -0600

    added point increase with difficulty increase

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
         # Bullet settings
         #   change width, speed, etc to test out features more easily
         #   or to create new powers for the ship
-        self.bullet_width = 5
+        self.bullet_width = 15
         self.bullet_height = 15
         self.bullet_color = (220, 20, 60)
         self.bullets_allowed = 3
@@ -24,6 +24,9 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+
+        # How quick;y the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -39,7 +42,11 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        # Below to check if points are increasing
+        print(self.alien_points)
